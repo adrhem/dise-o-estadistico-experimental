6c1507c953663ff7d6b65c42a73f59fb9dfccc2a
Adrián HM <adrian.otter@proton.me>
2025-11-20 14:48:33 -0600

    Tarea 9: Distribución de variables continuas

---
+++ tarea9/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from operaciones import Operaciones
+
+# Headers ['spotify_id', 'name', 'artists', 'daily_rank', 
+#          'daily_movement', 'weekly_movement', 'country', 
+#          'snapshot_date', 'popularity', 'is_explicit',
+#          'duration_ms', 'album_name', 'album_release_date', 
+#          'danceability', 'energy', 'key', 'loudness', 'mode', 
+#          'speechiness', 'acousticness', 'instrumentalness', 
+#          'liveness', 'valence', 'tempo', 'time_signature']
+
+data = pd.read_csv('../spotify_mx.csv')
+
+Operaciones.variables_continuas(data)
---
+++ tarea9/operaciones.py
@@ -0,0 +1,25 @@
+import scipy.stats as stats
+import numpy as np
+class Operaciones:
+
+    @staticmethod
+    def variables_continuas(data):
+        # Seleccionamos la variable continua 'danceability'
+        danceability = data['danceability']
+
+        # Calculamos la media y desviación estándar
+        mean = np.mean(danceability)
+        std_dev = np.std(danceability)
+
+        # Probabilidad de que la danceabilidad esté entre 0 y 0.3
+        prob_0_03 = stats.norm.cdf(0.3, mean, std_dev) - stats.norm.cdf(0, mean, std_dev)
+        print(f"Probabilidad de que la danceabilidad esté entre 0 y 0.3: {(prob_0_03 * 100):.4f}%")
+
+        # Probabilidad de que la danceabilidad esté entre 0.3 y 0.6
+        prob_03_06 = stats.norm.cdf(0.6, mean, std_dev) - stats.norm.cdf(0.3, mean, std_dev)
+        print(f"Probabilidad de que la danceabilidad esté entre 0.3 y 0.6: {(prob_03_06 * 100):.4f}%")
+        
+        # Probabilidad de que la danceabilidad sea mayor a 0.6
+        prob_greater_06 = 1 - stats.norm.cdf(0.6, mean, std_dev)
+        print(f"Probabilidad de que la danceabilidad sea mayor a 0.6: {(prob_greater_06 * 100):.4f}%")
+        
