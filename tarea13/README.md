b9881908667e0b990b1037155cf6fe3afd17334b
Adrián HM <adrian.otter@proton.me>
2025-11-25 15:20:54 -0600

    area 13: Análisis de bondad de ajuste de una distribución normal

--- tarea13/README.md
+++ tarea13/README.md
@@ -7,7 +7,15 @@ python main.py
 
 ### Salida del programa
 ```
+Media: 0.730464096749811, Desviación estándar: 0.1128989899284073
 
+Kolmogorov-Smirnov Test statistic: 0.1390686189258447, p-value: 0.0
+Rechazamos la hipótesis nula. Los datos no siguen una distribución normal.
 ```
 
 ### Análisis de resultados
+El análisis de bondad de ajuste se realizó sobre la variable "danceability" de la base de datos de Spotify México. La media y la desviación estándar calculadas son 0.7305 y 0.1129, respectivamente, lo que proporciona una idea general de la distribución de esta característica musical.
+
+El test de Kolmogorov-Smirnov arrojó un estadístico de 0.1391 y un p-valor de 0.0, lo que nos lleva a rechazar la hipótesis nula de que los datos siguen una distribución normal. Esto indica que la variable "danceability" no se ajusta a una distribución normal, lo cual es importante considerar en futuros análisis estadísticos, ya que muchos métodos asumen normalidad en los datos. Este resultado sugiere que se deben explorar otras distribuciones o métodos no paramétricos para analizar adecuadamente esta variable.
+
+
--- tarea13/operaciones.py
+++ tarea13/operaciones.py
@@ -1,12 +1,24 @@
+from scipy import stats
+import numpy as np
 
 class Operaciones:
 
     @staticmethod
     def analisis_bondad(data):
-        """
-        Selecciona una variable que consideres se ajusta a una distribución estándar, tu hipótesis.
-        Comprueba el ajuste a la distribución.
-        Aplica las medidas de distribución para determinar si hay asimetría o curtosis.
-        Explica los resultados y cómo estos afectan a tu objetivo de investigación.
-        """
-        print("Text")
+        # Para confirmar que los datos de "danceability" siguen una distribución normal,
+        # se puede utilizar la prueba de bondad de ajuste de Kolmogorov-Smirnov.
+        
+        # H0: Los datos siguen una distribución normal.
+        # H1: Los datos no siguen una distribución normal.
+
+        alpha = 0.05
+        datos = data['danceability'].dropna()
+        mu, std = np.mean(datos), np.std(datos, ddof=1)
+        print(f"Media: {mu}, Desviación estándar: {std}")
+        stat, p_value = stats.kstest(datos, 'norm', args=(mu, std))
+        print(f"\nKolmogorov-Smirnov Test statistic: {stat}, p-value: {p_value}")
+
+        if p_value < alpha:
+            print("Rechazamos la hipótesis nula. Los datos no siguen una distribución normal.")
+        else:
+            print("No rechazamos la hipótesis nula. Los datos siguen una distribución normal.")
