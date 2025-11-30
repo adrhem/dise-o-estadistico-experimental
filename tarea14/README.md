5caff6c5d9b78bf83454af44894038bcf92c4a38
Adrián HM <adrian.otter@proton.me>
2025-11-29 18:32:45 -0600

    Tarea 14: Análisis estadístico

--- tarea14/README.md
+++ tarea14/README.md
@@ -1,5 +1,7 @@
 ## Tarea 14: Análisis estadístico
 
+¿Qué tan variable es el atributo de "danceability" (bailabilidad) entre las canciones más populares en Spotify en México a lo largo del tiempo? 
+
 Para ejecutar el código, usa el siguiente comando en la terminal:
 ```bash
 python main.py
@@ -7,7 +9,16 @@ python main.py
 
 ### Salida del programa
 ```
-
+Promedio "Danceability": 0.730464096749811
+Desviación estándar de "Danceability": 0.1128989899284073
+Valor p de la prueba de normalidad: 0.0
+Rechazar H0: La distribución de "danceability" no es normal.
+Conclusión: Hay alta variabilidad en la "danceability" de las canciones populares en Spotify en México a lo largo del tiempo.
 ```
 
 ### Análisis de resultados
+Analizando los resultados obtenidos del análisis estadístico, se observa que el valor p de la prueba de normalidad es 0.0, lo que indica que se rechaza la hipótesis nula (H0) de que la distribución de "danceability" es normal. Esto sugiere que la variabilidad en el atributo de "danceability" entre las canciones más populares en Spotify en México a lo largo del tiempo es alta. Esta alta variabilidad puede reflejar una diversidad significativa en las características musicales de las canciones que alcanzan popularidad en la plataforma, lo que podría estar influenciado por diversos factores culturales, demográficos y de tendencias musicales en México.
+
+Esto a su vez implica que los gustos musicales en México son variados y que las canciones populares no se limitan a un solo estilo o característica musical, sino que abarcan una amplia gama de atributos que contribuyen a su popularidad. Este hallazgo es relevante para comprender mejor las preferencias musicales en el país y puede ser útil para artistas, productores y plataformas de streaming al momento de tomar decisiones relacionadas con la promoción y distribución de música.
+
+Además observando la gráfica de distribución de "danceability", se puede notar que la distribución no sigue una forma normal, lo que refuerza la conclusión de alta variabilidad en este atributo. En resumen, el análisis estadístico indica que la "danceability" de las canciones populares en Spotify en México es altamente variable, reflejando una diversidad musical significativa en el país.
--- tarea14/operaciones.py
+++ tarea14/operaciones.py
@@ -1,12 +1,47 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
 
 class Operaciones:
 
     @staticmethod
     def analisis_estadistico(data):
         """
-        Realiza las pruebas necesarias a tu objetivo de investigación siguiendo los 10 pasos. 
-        Considera que es probable que ya lo hayas hecho total o parcialmente en alguna de las tareas.
-        Recuerda que tu objetivo u objetivos son las hipótesis que deben guiar este análisis.
-        Explica los resultados y genera al menos una gráfica representativa de los mismos, esto serían tus conclusiones.
+        ¿Qué tan variable es el atributo de "danceability" (bailabilidad) 
+        entre las canciones más populares en Spotify en México a lo largo del tiempo? 
         """
-        print("Text")
+
+        # H0: La variabilidad de "danceability" es baja entre las canciones más populares.
+        # H1: La variabilidad de "danceability" es alta entre las canciones más populares.
+
+        alpha = 0.05
+
+        danceability_data = data['danceability']
+
+        mean_danceability = danceability_data.mean()
+        std_danceability = danceability_data.std()
+        print(f'Promedio "Danceability": {mean_danceability}')
+        print(f'Desviación estándar de "Danceability": {std_danceability}')
+
+        # Paso 5: Visualización de datos
+        plt.figure(figsize=(10, 6))
+        sns.histplot(danceability_data, bins=30, kde=True)
+        plt.title('Distribución de "Danceability"')
+        plt.xlabel('"Danceability"')
+        plt.ylabel('Frecuencia')
+        plt.show()
+
+        k2, p_value = stats.normaltest(danceability_data)
+        print(f'Valor p de la prueba de normalidad: {p_value}')
+
+        if p_value < alpha:
+            print("Rechazar H0: La distribución de \"danceability\" no es normal.")
+            variability = 'high'
+        else:
+            print("No rechazar H0: La distribución de \"danceability\" es normal.")
+            variability = 'low'
+
+        if variability == 'high':
+            print("Conclusión: Hay alta variabilidad en la \"danceability\" de las canciones populares en Spotify en México a lo largo del tiempo.")
+        else:
+            print("Conclusión: Hay baja variabilidad en la \"danceability\" de las canciones populares en Spotify en México a lo largo del tiempo.")
